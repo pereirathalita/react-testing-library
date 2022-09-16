f05b61be8ecb0bf1cdeef6e34d22e51fd2bbf76d
Thalita Pereira <105465339+pereirathalita@users.noreply.github.com>
2022-09-16 16:01:06 -0300

    feat: form

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,11 +1,29 @@
 import React from "react";
+import useState from "react";
+
 
 const ToDo = () => {
+    const [task, updateTask]=useState('')
+    const [tasks, updateTasks]=useState([])
+
+    // criando uma função para o recebimento do campo form
+    const handleInputChange = event => updateTask(event.target.value)
+    const handleFormSubmit = event => {
+        event.preventDefault()
+        updateTasks([...tasks, task])
+        updateTask('')
+    }
+
+    // criando um retorno com campos do form
     return (
         <>
-        <form>
-            <input/>
-            <button>Add new task</button>
+        <form onSubmit={handleFormSubmit}>
+            <input onChange={handleInputChange}
+            placeholder="Type a new task here"
+            type="text"
+            value={task}
+            />
+            <button type="submit">Add new task</button>
         </form>
         <table>
             <thead>
@@ -16,7 +34,6 @@ const ToDo = () => {
         </table>
         </>
     )
-
 }   
 
 export default ToDo;
