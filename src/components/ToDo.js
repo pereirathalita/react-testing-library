65141e699b8563f73e50e82206c9336931a5b0d0
Thalita Pereira <105465339+pereirathalita@users.noreply.github.com>
2022-09-15 21:34:10 -0300

    feat: init

---
+++ src/components/ToDo.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ToDo = () => {
+    return (
+        <>
+        <form>
+            <input/>
+            <button>Add new task</button>
+        </form>
+        <table>
+            <thead>
+                <tr>
+                    <th>Task</th>
+                </tr>
+            </thead>
+        </table>
+        </>
+    )
+
+}   
+
+export default ToDo;
---
+++ src/containers/App.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ToDo from '../components/ToDo';
+
+const App=() => <ToDo /> 
+
+export default App;
